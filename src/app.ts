f8b12fabad3b6ed73e09e4aae91de33abcd2924a
sachu0dev <sushil.dev.in@gmail.com>
2024-07-23 19:50:47 +0530

    install dependencies

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
+
+// routes
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript Node Express!");
+  res.send("Hello");
 });
 
 app.listen(port, () => {
