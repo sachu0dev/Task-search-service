ac8ffe1e4fea863415b7793a88cad0f48edb60fe
sachu0dev <sushil.dev.in@gmail.com>
2024-07-23 19:21:04 +0530

    intialize server

---
+++ src/middlewares/error.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+
+// Error middleware
+const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
+
+const TryCatch =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export { errorMiddleware, TryCatch };
